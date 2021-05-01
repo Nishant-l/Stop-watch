53469063260e4442930a8a1792d60896ee10d7a2
Nishant <nishant3151998@gmail.com>
2021-05-01 10:37:54 +0530

    ✨ adding reset button

--- app.js
+++ app.js
@@ -9,12 +9,19 @@ const second=display.querySelector('.seconds');
 const controls=document.querySelector('.controls');
 const startButton=controls.querySelector('.start');
 const stopButton=controls.querySelector('.stop');
+const resetButton=controls.querySelector('.reset');
 // Adding listeners
 startButton.addEventListener('click',start);
 stopButton.addEventListener('click',stop);
+resetButton.addEventListener('click',reset);
 // initializers
 render();
 // functions
+function reset(){
+    leftTIme=25*SECONDS_IN_MINUTES+0;
+    stop();
+    render();
+}
 function start(){
     if(!timer){
         timer=setInterval(tick,1000);
